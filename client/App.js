ea73392fa1eebf395ffd71643f6d225bef20498e
John Yu <johnyu916@hotmail.com>
2017-05-11 10:56:27 -0400

    added update

--- client/App.js
+++ client/App.js
@@ -6,11 +6,13 @@ import Base from './components/Layout/Base'
 import Header from './components/Header'
 import Main from './components/Main'
 import Find from './components/Find'
+import Update from './components/Update'
 render(
   (
     <Base>
       <Header />
       <Main />
       <Find />
+      <Update />
     </Base>
   ), document.getElementById('app'))
